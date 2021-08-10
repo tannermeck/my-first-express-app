2d6df2659719e6eb8fce7d5d5c0d9d7c68c90a66
TANNER MECK <steve.meck246@gmail.com>
2021-08-10 15:05:42 -0700

    data updated

--- __tests__/get.test.js
+++ __tests__/get.test.js
@@ -4,14 +4,14 @@ const request = supertest(app);
 
 const data = require('../data.js');
 
-it('gets the dogs endpoint', async () => {
+it('gets the dirtbike endpoint', async () => {
     const response = await request.get('/dirtbikes');
 
     expect(response.status).toBe(200);
     expect(response.body).toEqual(data);
 });
 it('gets the dirtbike show endpoint', async () => {
-    const response = await request.get('puppies/1');
+    const response = await request.get('/dirtbikes/1');
     expect(response.status).toBe(200);
     expect(response.body).toEqual(data[0])
 })
--- data.js
+++ data.js
@@ -2,10 +2,32 @@ const data = [
     {
         id: 1,
         brand: 'kawasaki',
+        dirtbike: true,
+        tires: 'dunlop'
     },
     {
         id: 2,
-        brand: 'honda'
+        brand: 'honda',
+        dirtbike: true,
+        tires: 'pirelli'
+    },
+    {
+        id: 3,
+        brand: 'yamaha',
+        dirtbike: true,
+        tires: 'pirelli'
+    },
+    {
+        id: 4,
+        brand: 'suzuki',
+        dirtbike: true,
+        tires: 'dunlop'
+    },
+    {
+        id: 5,
+        brand: 'ktm',
+        dirtbike: true,
+        tires: 'dunlop'
     },
 ]
 module.exports = data
